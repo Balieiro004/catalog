96a564bd519b535dc2a0461c5e9320a0f86a8dc7
Balieiro004 <Balieiro004@gmail.com>
2025-01-08 20:08:20 -0300

    Criando a clase category, configurando properties, e criando base de dados

---
+++ src/main/java/com/balieiro/Catalog/entities/Category.java
@@ -0,0 +1,17 @@
+package com.balieiro.Catalog.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
